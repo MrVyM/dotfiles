3b8c0976bba78a3d25822c337dc98ff199e30356
MrVyM <la.regle.du.jeu.ytb@gmail.com>
2023-05-17 12:13:13 +0200

    [I3LOCK] Create the script file to lock the session with the background

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,3 +1,9 @@
+nitrogen --set-zoom-fill --random /home/vym/.config/nitrogen/backgrounds --save
 FILE=$(cat /home/vym/.config/nitrogen/bg-saved.cfg | grep file | cut -d "=" -f2)
-convert "$FILE" "$FILE.png" 
-i3lock -i "$FILE.png"
+DESTINATION=$(echo $FILE | rev | cut -d "/" -f 3- | rev)
+FILE_DESTINATION="$DESTINATION/lock/"$(basename "$FILE")".png" 
+if [ ! -f "$FILE_DESTINATION" ]
+then
+    convert -blur 0x3 "$FILE" "$FILE_DESTINATION" 
+fi
+i3lock -i "$FILE_DESTINATION" 
