2aa9a31df4ea1d38fabd0c142501434d2442788a
vianney.marticou <la.regle.du.jeu.ytb@gmail.com>
2023-05-26 14:02:44 +0200

    [NVIM] Install markdown viewer and add some comment in plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,36 +2,38 @@
 require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
+    -- Git wrapper
     use 'tpope/vim-fugitive'
 
+    -- Telescope
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
+    use 'BurntSushi/ripgrep'
+    use 'nvim-treesitter/nvim-treesitter' 
 
+    -- Color RGB
+    use 'norcalli/nvim-colorizer.lua'
+
+    -- LSPconfig
     use "neovim/nvim-lspconfig"
     use "hrsh7th/cmp-nvim-lsp" 
-
-    use {
-        "williamboman/mason.nvim"
-    }
-
+    use "williamboman/mason.nvim"
     use "williamboman/mason-lspconfig.nvim"
-
-    use 'BurntSushi/ripgrep'
-
     use 'hrsh7th/nvim-compe' 
     use 'hrsh7th/nvim-cmp' 
 
-    use 'sharkdp/fd' 
-
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-    }
-    
+    -- Colorscheme
     use 'navarasu/onedark.nvim'
-
     use 'ray-x/aurora'
+
+    -- Markdown Viewer
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
+
+
 end)
 
